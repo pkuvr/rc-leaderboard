5d6872bfdc8998ec76278f1f19da96db6be02c11
jianzi0307 <jianzi0307@qq.com>
2018-06-16 01:24:30 +0800

    ecmascript6 class.

--- index.js
+++ index.js
@@ -3,222 +3,222 @@
 const LeaderBoard = require('./lib/leaderboard');
 const cron = require('node-cron');
 
-let RcLeaderBoard = function () {
-};
+class RcLeaderBoard {
+  constructor(host, port, options) {
+    options = options || null;
+    this.lb = new LeaderBoard();
+    this.lb.createClient(host, port, options);
 
-/**
- * [private]
- * 清除周期性排行榜
- * @param pattern
- * @param period
- * @returns {function(this:T)}
- */
-let clearPeriodLeaderBoard = (pattern, period) => {
-  let self = this;
-  return cron.schedule(pattern, function () {
-    self.lb.clearPeriodLeaderBoard(period, (err, res) => {
-      console.debug('clear period leaderboards.');
-    })
-  }.bind(this));
-};
-
-RcLeaderBoard.prototype.createClient = (host, port, options) => {
-  options = options || null;
-  this.lb = new LeaderBoard();
-  this.lb.createClient(host, port, options);
-};
-
-/**
- * 新增一个实体
- * @param entity
- * @param callback
- * @param options
- */
-RcLeaderBoard.prototype.add = (entity, callback, options) => {
-  entity = entity || {};
-  if (!entity.userId || !entity.attrName) {
-    callback('parameter error.');
-  }
-  entity.createdAt = entity.createdAt || new Date().toString();
-  entity.score = entity.score || 0;
-  entity.extra = entity.extra || '';
-
-  options = options || {};
-  options.group = options.group || 'default';
-  this.lb.add(entity, callback, options);
-};
-
-RcLeaderBoard.prototype.activePeriods = (options) => {
-  options = options || {};
-  let daily = options.daily || false,
-    weekly = options.weekly || false,
-    monthly = options.monthly || false,
-    yearly = options.yearly || false;
-  if (daily) {
-    this.dailyTask = clearPeriodLeaderBoard.call(this, '0 0 * * *');
-    this.lb.activePeriod('daily');
+    /**
+     * [private]
+     * 清除周期性排行榜
+     * @param pattern
+     * @param period
+     * @returns {function(this:T)}
+     */
+    this.clearPeriodLeaderBoard = (pattern, period) => {
+      let self = this;
+      return cron.schedule(pattern, function () {
+        self.lb.clearPeriodLeaderBoard(period, (err, res) => {
+          console.debug('clear period leaderboards.');
+        })
+      }.bind(this));
+    };
   }
 
-  if (weekly) {
-    this.weeklyTask = clearPeriodLeaderBoard.call(this, '0 0 * * 0');
-    this.lb.activePeriod('weekly');
+  /**
+   * 新增一个实体
+   * @param entity
+   * @param callback
+   * @param options
+   */
+  add(entity, callback, options) {
+    entity = entity || {};
+    if (!entity.userId || !entity.attrName) {
+      callback('parameter error.');
+    }
+    entity.createdAt = entity.createdAt || new Date().toString();
+    entity.score = entity.score || 0;
+    entity.extra = entity.extra || '';
+
+    options = options || {};
+    options.group = options.group || 'default';
+    this.lb.add(entity, callback, options);
   }
 
-  if (monthly) {
-    this.monthlyTask = clearPeriodLeaderBoard.call(this, '0 0 1 * *');
-    this.lb.activePeriod('monthly');
+  activePeriods(options) {
+    options = options || {};
+    let daily = options.daily || false,
+      weekly = options.weekly || false,
+      monthly = options.monthly || false,
+      yearly = options.yearly || false;
+    if (daily) {
+      this.dailyTask = this.clearPeriodLeaderBoard.call(this, '0 0 * * *');
+      this.lb.activePeriod('daily');
+    }
+
+    if (weekly) {
+      this.weeklyTask = this.clearPeriodLeaderBoard.call(this, '0 0 * * 0');
+      this.lb.activePeriod('weekly');
+    }
+
+    if (monthly) {
+      this.monthlyTask = this.clearPeriodLeaderBoard.call(this, '0 0 1 * *');
+      this.lb.activePeriod('monthly');
+    }
+
+    if (yearly) {
+      this.yearlyTask = this.clearPeriodLeaderBoard.call(this, ' 0 0 1 1 *');
+      this.lb.activePeriod('yearly');
+    }
   }
 
-  if (yearly) {
-    this.yearlyTask = clearPeriodLeaderBoard.call(this, ' 0 0 1 1 *');
-    this.lb.activePeriod('yearly');
-  }
-};
-
-/**
- * 获取排行榜
- * @param callback
- * @param options
- */
-RcLeaderBoard.prototype.getLeaderboard = (callback, options) => {
-  options = options || {};
-  let group = options.group || 'default',
-    period = options.period || 'alltime',
-    attrName = options.attrName || null,
-    from = options.from || 0,
-    to = options.to || -1;
-  if (!userId || !attrName) {
-    callback('params error.');
-    return;
-  }
-  this.lb.getLeaderboard(group, period, attrName, from, to, callback);
-};
-
-/**
- * 获取top榜，例如top100
- * @param top
- * @param callback
- * @param options
- */
-RcLeaderBoard.prototype.getTop = (top, callback, options) => {
-  options = options || {};
-  let group = options.group || 'default',
-    period = options.period || 'alltime',
-    attrName = options.attrName || null;
-  if (!attrName) {
-    callback('params error.');
-    return;
-  }
-  this.lb.getLeaderboard(group, period, attrName, 1, top, callback);
-};
-
-/**
- * 获取用户上下范围的排行榜
- * @param userId
- * @param callback
- * @param options
- */
-RcLeaderBoard.prototype.getAroundUserLeaderboard = (userId, callback, options) => {
-  options = options || {};
-  let group = options.group || 'default',
-    period = options.period || 'alltime',
-    attrName = options.attrName || null,
-    range = options.range || 10;
-  if (!userId || !attrName) {
-    callback('params error.');
-    return;
-  }
-  this.lb.getAroundUserLeaderboard(group, period, userId, attrName, range, callback);
-};
-
-
-/**
- * 获取用户最高分
- * @param userId
- * @param callback
- * @param options
- * @param filterOptions
- */
-RcLeaderBoard.prototype.getBestScore = (userId, callback, options, filterOptions) => {
-  options = options || {};
-  let group = options.group || 'default',
-    period = options.period || 'alltime',
-    attrName = options.attrName || null;
-  if (!userId || !attrName) {
-    callback('params error.');
-    return;
-  }
-  this.lb.getUserBestScore(group, period, userId, attrName, callback, filterOptions);
-};
-
-/**
- * 获取用户累计分
- * @param userId
- * @param callback
- * @param options
- */
-RcLeaderBoard.prototype.getTotalScore = (userId, callback, options) => {
-  options = options || {};
-  let group = options.group || 'default',
-    period = options.period || 'alltime',
-    attrName = options.attrName || null;
-  if (!userId || !attrName) {
-    callback('params error.');
-    return;
-  }
-  this.lb.getUserTotalScore(group, period, userId, attrName, callback);
-};
-
-/**
- * 获取用户排名
- * @param userId
- * @param callback
- * @param options
- */
-RcLeaderBoard.prototype.getRank = (userId, callback, options) => {
-  options = options || {};
-  let group = options.group || 'default',
-    period = options.period || 'alltime',
-    attrName = options.attrName || null;
-  if (!userId || !attrName) {
-    callback('params error.');
-    return;
-  }
-  this.lb.getRank(group, period, userId, attrName, callback);
-};
-
-RcLeaderBoard.prototype.flushAll = () => {
-  this.lb.flushAll();
-};
-
-RcLeaderBoard.prototype.removeLeaderboards = (options) => {
-  options = options || {};
-  let
-    group = options.group || 'default',
-    daily = options.daily || false,
-    weekly = options.weekly || false,
-    monthly = options.monthly || false,
-    yearly = options.yearly || false;
-
-  if (daily && !!this.dailyTask) {
-    this.dailyTask.destroy();
-    this.lb.clearPeriodLeaderBoard(group, 'daily');
+  /**
+   * 获取排行榜
+   * @param callback
+   * @param options
+   */
+  getLeaderboard(callback, options) {
+    options = options || {};
+    let group = options.group || 'default',
+      period = options.period || 'alltime',
+      attrName = options.attrName || null,
+      from = options.from || 0,
+      to = options.to || -1;
+    if (!attrName) {
+      callback('params error.');
+      return;
+    }
+    this.lb.getLeaderboard(group, period, attrName, from, to, callback);
   }
 
-  if (weekly && !!this.weeklyTask) {
-    this.weeklyTask.destroy();
-    this.lb.clearPeriodLeaderBoard(group, 'weekly');
+  /**
+   * 获取top榜，例如top100
+   * @param top
+   * @param callback
+   * @param options
+   */
+  getTop(top, callback, options) {
+    options = options || {};
+    let group = options.group || 'default',
+      period = options.period || 'alltime',
+      attrName = options.attrName || null;
+    if (!attrName) {
+      callback('params error.');
+      return;
+    }
+    this.lb.getLeaderboard(group, period, attrName, 1, top, callback);
   }
 
-  if (monthly && !!this.monthlyTask) {
-    this.monthlyTask.destroy();
-    this.lb.clearPeriodLeaderBoard(group, 'monthly');
+  /**
+   * 获取用户上下范围的排行榜
+   * @param userId
+   * @param callback
+   * @param options
+   */
+  getAroundUserLeaderboard(userId, callback, options) {
+    options = options || {};
+    let group = options.group || 'default',
+      period = options.period || 'alltime',
+      attrName = options.attrName || null,
+      range = options.range || 10;
+    if (!userId || !attrName) {
+      callback('params error.');
+      return;
+    }
+    this.lb.getAroundUserLeaderboard(group, period, userId, attrName, range, callback);
   }
 
-  if (yearly && !!this.yearlyTask) {
-    this.yearlyTask.destroy();
-    this.lb.clearPeriodLeaderBoard(group, 'yearly');
+  /**
+   * 获取用户最高分
+   * @param userId
+   * @param callback
+   * @param options
+   * @param filterOptions
+   */
+  getBestScore(userId, callback, options, filterOptions) {
+    options = options || {};
+    let group = options.group || 'default',
+      period = options.period || 'alltime',
+      attrName = options.attrName || null;
+    if (!userId || !attrName) {
+      callback('params error.');
+      return;
+    }
+    this.lb.getUserBestScore(group, period, userId, attrName, callback, filterOptions);
   }
-};
+
+  /**
+   * 获取用户累计分
+   * @param userId
+   * @param callback
+   * @param options
+   */
+  getTotalScore(userId, callback, options) {
+    options = options || {};
+    let group = options.group || 'default',
+      period = options.period || 'alltime',
+      attrName = options.attrName || null;
+    if (!userId || !attrName) {
+      callback('params error.');
+      return;
+    }
+    this.lb.getUserTotalScore(group, period, userId, attrName, callback);
+  }
+
+
+  /**
+   * 获取用户排名
+   * @param userId
+   * @param callback
+   * @param options
+   */
+  getRank(userId, callback, options) {
+    options = options || {};
+    let group = options.group || 'default',
+      period = options.period || 'alltime',
+      attrName = options.attrName || null;
+    if (!userId || !attrName) {
+      callback('params error.');
+      return;
+    }
+    this.lb.getRank(group, period, userId, attrName, callback);
+  }
+
+  flushAll() {
+    this.lb.flushAll();
+  }
+
+  removeLeaderboards(options) {
+    options = options || {};
+    let
+      group = options.group || 'default',
+      daily = options.daily || false,
+      weekly = options.weekly || false,
+      monthly = options.monthly || false,
+      yearly = options.yearly || false;
+
+    if (daily && !!this.dailyTask) {
+      this.dailyTask.destroy();
+      this.lb.clearPeriodLeaderBoard(group, 'daily');
+    }
+
+    if (weekly && !!this.weeklyTask) {
+      this.weeklyTask.destroy();
+      this.lb.clearPeriodLeaderBoard(group, 'weekly');
+    }
+
+    if (monthly && !!this.monthlyTask) {
+      this.monthlyTask.destroy();
+      this.lb.clearPeriodLeaderBoard(group, 'monthly');
+    }
+
+    if (yearly && !!this.yearlyTask) {
+      this.yearlyTask.destroy();
+      this.lb.clearPeriodLeaderBoard(group, 'yearly');
+    }
+  }
+}
+
 
 module.exports = RcLeaderBoard;
